ac19207efba0b78de8191d114e4d67465ecf5702
AvazAlimov <avazalimov95@mail.ru>
2018-06-13 14:02:48 +0500

    Avaz Committed

---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+process.env.MONGO_ATLAS_PASSWORD = process.env.MONGO_ATLAS_PASSWORD || "%40v%40zARS23";
+process.env.JWT_KEY = process.env.JWT_KEY || "secret";
+process.env.PORT = process.env.PORT || 3000;
+process.env.ADMIN_KEY = process.env.ADMIN_KEY || "$2y$12$F18oA5DbySBtqeXPdW1co.hgqmYS17dqTi079BhIJmvYV7ItFNfHG";
