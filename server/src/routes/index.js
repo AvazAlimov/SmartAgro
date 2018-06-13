0b9dd314454e41119263b6d3ad7fa1bb56ce8812
AvazAlimov <avazalimov95@mail.ru>
2018-06-13 10:38:26 +0500

    Avaz Committed

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,11 @@
 const user = require("./user");
 const admin = require("./admin");
+const station = require("./station");
+const data = require("./data");
 
 module.exports = app => {
   app.use("/admin", admin);
   app.use("/user", user);
+  app.use("/station", station);
+  app.use("/data", data);
 };
