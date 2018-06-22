9f0d9a97b082bc599977fd81d03e424e26c4438b
AvazAlimov <avazalimov95@mail.ru>
2018-06-22 14:51:29 +0500

    Avaz Commited

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default () => {
-  // axios.defaults.headers.post["Content-Type"] = "application/json";
   return axios.create({
     baseURL:
       process.env.NODE_ENV === "production"
